4d2e17390fae38a53b3a26f9dab3fb337056a667
lab_services_student <menziwokuhlecele@gmail.com>
2022-08-02 11:31:28 +0200

    Another One. DJ Commit

--- app/src/main/java/com/menzicele/simpleweather2022/MainActivity.java
+++ app/src/main/java/com/menzicele/simpleweather2022/MainActivity.java
@@ -1,6 +1,9 @@
 package com.menzicele.simpleweather2022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +13,21 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     public final String TAG = "AccuweatherURL";
+    Fragment FiveDayWeather;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FiveDayWeather = new Fragment();
+
         URL accuweatherURL = NetworkUtil.buildURL();
         Log.i(TAG, "onCreate: " + accuweatherURL);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.weather_frame, FiveDayWeather);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/menzicele/simpleweather2022/NetworkUtil.java
+++ app/src/main/java/com/menzicele/simpleweather2022/NetworkUtil.java
@@ -1,9 +1,16 @@
 package com.menzicele.simpleweather2022;
 
 import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class NetworkUtil
 {
@@ -12,6 +19,7 @@ public class NetworkUtil
     private static final String PARAM_API_KEY = "apikey";
     private static final String METRIC_PARAM = "metric";
     private static final String METRIC_VALUE = "true";
+    private static String TAG = "NETWORK_UTIL";
 
     public static URL buildURL()
     {
@@ -30,5 +38,30 @@ public class NetworkUtil
         }
 
         return url;
+    }
+
+    public static String getResponse(URL url) throws IOException
+    {
+        HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+        
+        try {
+            InputStream in = httpsURLConnection.getInputStream();
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("//A");
+            boolean hasInput = scanner.hasNext();
+            
+            if(hasInput)
+            {
+                return scanner.next();
+            }
+            else
+            {
+                Log.i(TAG, "getResponse: "+scanner.next());
+                return null;
+            }
+        }
+        finally{
+            httpsURLConnection.disconnect();
+        }
     }
 }
